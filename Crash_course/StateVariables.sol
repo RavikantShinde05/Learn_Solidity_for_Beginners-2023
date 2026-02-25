c0f5ff784069e8519551b0d34cac3cde23049bc9
RevX05@ <117154752+RavikantShinde05@users.noreply.github.com>
2026-02-25 21:48:57 +0530

    Add files via upload

---
+++ Crash_course/StateVariables.sol
@@ -0,0 +1,11 @@
+// SPDX-License-identifier: Unlicense
+pragma solidity ^0.8.9;
+
+// these are some common State variables.
+contract Mycontract {
+    string public name = "Harry Potter";
+    int public age = 25;
+    uint public balance = 2500;
+    bool public eligible = true;
+    address public myAddress = 0xa1f65as4f651as3fa31f4af165s1f4asrf4616f5s16;
+}
